4edf82d36d9e3d5c3deecaf9b7e9a796beaea31f
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-09 00:59:42 +0100

    DNDClient: Added interface and new methods to fetch Result and Detail

--- src/main/java/app/integrations/DNDClient.java
+++ src/main/java/app/integrations/DNDClient.java
@@ -1,9 +1,7 @@
 package app.integrations;
 
-import app.dtos.DNDRaceDTO;
-import app.dtos.DNDRaceResultDTO;
+import app.dtos.*;
 import app.exceptions.ApiException;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -12,14 +10,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class DNDClient
+public class DNDClient implements IDNDClient
 {
     private HttpClient client;
     private ObjectMapper objectMapper;
-
-    private static final String BASE_URL = "https://www.dnd5eapi.co/api/2014%s";
-    private static final String ALL_RACES_URL = String.format(BASE_URL, "/races");
-
 
     public DNDClient(HttpClient client, ObjectMapper objectMapper)
     {
@@ -27,9 +21,10 @@ public class DNDClient
         this.objectMapper = objectMapper;
     }
 
-    public DNDRaceResultDTO getAllRaces()
+    @Override
+    public DNDRaceResultDTO fetchAllRaces(String url)
     {
-        HttpRequest request = buildRequest(ALL_RACES_URL);
+        HttpRequest request = buildRequest(url);
         try
         {
             String response = getResponse(request);
@@ -42,20 +37,116 @@ public class DNDClient
         }
     }
 
-    // TODO Remove or refactor
-    private DNDRaceDTO mapRaceDto(String json)
+    @Override
+    public DNDSubraceResultDTO fetchAllSubraces(String url)
     {
-        DNDRaceDTO dndRaceDTO = null;
-
+        HttpRequest request = buildRequest(url);
         try
         {
-            dndRaceDTO = objectMapper.readValue(json, DNDRaceDTO.class);
+            String response = getResponse(request);
+            DNDSubraceResultDTO dndSubraceResultDTO = objectMapper.readValue(response, DNDSubraceResultDTO.class);
+            return dndSubraceResultDTO;
         }
-        catch (JsonProcessingException e)
+        catch (IOException | InterruptedException e)
         {
             throw new RuntimeException(e);
         }
-        return dndRaceDTO;
+    }
+
+    @Override
+    public DNDTraitResultDTO fetchAllTraits(String url)
+    {
+        HttpRequest request = buildRequest(url);
+        try
+        {
+            String response = getResponse(request);
+            DNDTraitResultDTO dndTraitResultDTO = objectMapper.readValue(response, DNDTraitResultDTO.class);
+            return dndTraitResultDTO;
+        }
+        catch (IOException | InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public DNDLanguageResultDTO fetchAllLanguages(String url)
+    {
+        HttpRequest request = buildRequest(url);
+        try
+        {
+            String response = getResponse(request);
+            DNDLanguageResultDTO dndLanguageResultDTO = objectMapper.readValue(response, DNDLanguageResultDTO.class);
+            return dndLanguageResultDTO;
+        }
+        catch (IOException | InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public DNDRaceDetailDTO fetchRaceDetails(String url)
+    {
+        HttpRequest request = buildRequest(url);
+        try
+        {
+            String response = getResponse(request);
+            DNDRaceDetailDTO dndRaceDetailDTO = objectMapper.readValue(response, DNDRaceDetailDTO.class);
+            return dndRaceDetailDTO;
+        }
+        catch (IOException | InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public DNDSubraceDetailDTO fetchSubraceDetails(String url)
+    {
+        HttpRequest request = buildRequest(url);
+        try
+        {
+            String response = getResponse(request);
+            DNDSubraceDetailDTO dndSubraceDetailDTO = objectMapper.readValue(response, DNDSubraceDetailDTO.class);
+            return dndSubraceDetailDTO;
+        }
+        catch (IOException | InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public DNDTraitDetailDTO fetchTraitDetails(String url)
+    {
+        HttpRequest request = buildRequest(url);
+        try
+        {
+            String response = getResponse(request);
+            DNDTraitDetailDTO dndTraitDetailDTO = objectMapper.readValue(response, DNDTraitDetailDTO.class);
+            return dndTraitDetailDTO;
+        }
+        catch (IOException | InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public DNDLanguageDetailDTO fetchLanguageDetails(String url)
+    {
+        HttpRequest request = buildRequest(url);
+        try
+        {
+            String response = getResponse(request);
+            DNDLanguageDetailDTO dndLanguageDetailDTO = objectMapper.readValue(response, DNDLanguageDetailDTO.class);
+            return dndLanguageDetailDTO;
+        }
+        catch (IOException | InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     private String getResponse(HttpRequest request) throws IOException, InterruptedException
@@ -63,7 +154,7 @@ public class DNDClient
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         if (response.statusCode() != 200)
         {
-            throw new ApiException(response.statusCode(), "dnd5eapi returned error code: " + response.statusCode());
+            throw new ApiException(response.statusCode(), "DnD5Eapi returned error code: " + response.statusCode());
         }
         return response.body();
     }
