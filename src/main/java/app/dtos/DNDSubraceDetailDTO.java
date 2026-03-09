61d57f7eec3dd11838df0acdff3cfec365fc2ec9
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-09 01:01:46 +0100

    Added DND Subrace Result and Detail DTOs

---
+++ src/main/java/app/dtos/DNDSubraceDetailDTO.java
@@ -0,0 +1,36 @@
+package app.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.Instant;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DNDSubraceDetailDTO(
+        @JsonProperty("index")
+        String index,
+
+        @JsonProperty("name")
+        String name,
+
+        @JsonProperty("race")
+        DNDRaceDTO race,
+
+        @JsonProperty("desc")
+        String description,
+
+        @JsonProperty("ability_bonuses")
+        List<DNDAbilityBonusDTO> abilityBonuses,
+
+        @JsonProperty("racial_traits")
+        List<DNDTraitDTO> racialTraits,
+
+        @JsonProperty("url")
+        String url,
+
+        @JsonProperty("updated_at")
+        Instant updatedAt
+)
+{
+}
