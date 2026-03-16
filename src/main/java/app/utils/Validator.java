af93c77669c07cfb65d38fa29b95c83434666880
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-16 09:55:37 +0100

    Added Validator util class to project

---
+++ src/main/java/app/utils/Validator.java
@@ -0,0 +1,38 @@
+package app.utils;
+
+import java.util.List;
+
+public class Validator
+{
+    public static <T> void notNull(T object)
+    {
+        if (object == null)
+        {
+            throw new IllegalArgumentException("Input cannot be null");
+        }
+    }
+
+    public static void notBlank(String value)
+    {
+        if (value == null || value.isEmpty())
+        {
+            throw new IllegalArgumentException("String cannot be null or blank");
+        }
+    }
+
+    public static <T> void notEmpty(List<T> objects)
+    {
+        if (objects == null || objects.isEmpty())
+        {
+            throw new IllegalArgumentException("List cannot be null or empty");
+        }
+    }
+
+    public static void validId(Long id)
+    {
+        if (id == null || id <= 0)
+        {
+            throw new IllegalArgumentException("Invalid ID");
+        }
+    }
+}
