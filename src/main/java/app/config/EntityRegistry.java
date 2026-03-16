6c97eaff158fee594c988e9df10cc9a341c3f39d
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-16 09:56:54 +0100

    EntityRegistry: Added entities

--- src/main/java/app/config/EntityRegistry.java
+++ src/main/java/app/config/EntityRegistry.java
@@ -4,6 +4,10 @@ import app.entities.Campaign;
 import app.entities.CampaignMembership;
 import app.entities.CharacterSheet;
 import app.entities.User;
+import app.entities.reference.Language;
+import app.entities.reference.Race;
+import app.entities.reference.Subrace;
+import app.entities.reference.Trait;
 import org.hibernate.cfg.Configuration;
 
 final class EntityRegistry {
@@ -15,5 +19,9 @@ final class EntityRegistry {
         configuration.addAnnotatedClass(CharacterSheet.class);
         configuration.addAnnotatedClass(Campaign.class);
         configuration.addAnnotatedClass(CampaignMembership.class);
+        configuration.addAnnotatedClass(Race.class);
+        configuration.addAnnotatedClass(Subrace.class);
+        configuration.addAnnotatedClass(Language.class);
+        configuration.addAnnotatedClass(Trait.class);
     }
 }
