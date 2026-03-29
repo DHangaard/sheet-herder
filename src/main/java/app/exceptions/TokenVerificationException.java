721cbfb28a2212153306192e2eb37bf0c05e2a84
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-03-30 01:06:31 +0200

    TokenVerificationException added to project

---
+++ src/main/java/app/exceptions/TokenVerificationException.java
@@ -0,0 +1,10 @@
+package app.exceptions;
+
+public class TokenVerificationException extends Exception
+{
+
+    public TokenVerificationException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+}
